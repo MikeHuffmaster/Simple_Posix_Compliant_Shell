a7ffdc2d6311997e104a682ea1b67eb8276aba59
Mike Huffmaster <112338906+MikeHuffmaster@users.noreply.github.com>
2024-12-04 23:20:32 +0000

    trying unsetenv for unset

--- src/builtins.c
+++ src/builtins.c
@@ -211,6 +211,11 @@ builtin_unset(struct command *cmd, struct builtin_redir const *redir_list)
   {
     if (cmd->words[i] != NULL)
     {
+      if (unsetenv(cmd->words[i]) < 0)
+      {
+        dprintf(get_pseudo_fd(redir_list, STDIN_FILENO), "Cannot unset variable\n");
+      }
+
       free(cmd->words[i]); // use free to unset the variables and the set them to NULL
       cmd->words[i] = NULL;
     }
